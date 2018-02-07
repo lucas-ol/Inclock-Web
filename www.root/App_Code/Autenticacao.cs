735583ee933b805ce93470ab49dd749a84b94063
MrBuble <oliveiramelo1996@gmail.com>
2018-02-07 20:12:55 -0200

    Adicionado a pasta App_Code e a classe para autenticao do usuario(Não implementada)

---
+++ www.root/App_Code/Autenticacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+/// <summary>
+/// Descrição resumida de Autenticacao
+/// </summary>
+public class Autenticacao: Page
+{
+    public Autenticacao()
+    {
+       
+    }
+}
--- www.root/avisos/Default.aspx.cs
+++ www.root/avisos/Default.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class noticia_Default : Autenticador
+public partial class noticia_Default : Autenticacao
 {
     public int TipoPagina
     {
