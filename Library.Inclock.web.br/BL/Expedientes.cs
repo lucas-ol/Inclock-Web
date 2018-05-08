c71e0d24f1e0bb5eb3d55da71e680ba98411b81e
MrBuble <oliveiramelo1996@gmail.com>
2018-05-08 00:17:08 -0300

    Ajustado o update do Expedientes

--- Library.Inclock.web.br/BL/Expedientes.cs
+++ Library.Inclock.web.br/BL/Expedientes.cs
@@ -13,18 +13,31 @@ namespace Library.Inclock.web.br.BL
     {
         public FeedBack SalvaExpediente(Classes.VO.Expediente expediente)
         {
-            FeedBack feedback = new FeedBack() { Status = false };
+            FeedBack feedBack = new FeedBack() { Status = false };
             MySqlAdicionaParametro("_saida", expediente.Saida);
             MySqlAdicionaParametro("_entrada", expediente.Entrada);
             MySqlAdicionaParametro("_semanaEntrada", expediente.DiaSemana);
             MySqlAdicionaParametro("_semanaSaida", CheckSaida(expediente));
             MySqlAdicionaParametro("_periodo", expediente.Periodo);
             MySqlAdicionaParametro("_funcionario_id", expediente.Funcionario_id);
-            feedback = MySqlExecutaComando("prd_insert_expediente", System.Data.CommandType.StoredProcedure);
-            int retorno;
-            int.TryParse(feedback.Mensagem, out retorno);
-            feedback.Status = retorno > 0;
-            return feedback; ;
+            feedBack = MySqlExecutaComando("prd_insert_expediente", System.Data.CommandType.StoredProcedure);
+            if (feedBack.Mensagem.Contains("integral"))
+            {
+                feedBack.Mensagem = "Nesse expediente tem um periodo integral";
+                feedBack.Status = false;
+            }
+            else if (feedBack.Mensagem.Contains("duplicate"))
+            {
+                feedBack.Status = false;
+                feedBack.Mensagem = "Expediente ja cadastrado";
+            }
+            else if (feedBack.Mensagem.Contains("expediente"))
+            {
+                feedBack.Status = false;
+                feedBack.Mensagem = "Você não pode cadastrar um expediente integral no dia que ja tem outros expedientes cadastrados";
+            }
+
+            return feedBack; ;
         }
         public List<Expediente> ListaExpediente(int funcionario_Id)
         {
@@ -42,6 +55,7 @@ namespace Library.Inclock.web.br.BL
             expediente.AddRange(responce);
             return expediente;
         }
+        
         public FeedBack AtualizaExpediente(Expediente expediente)
         {
             FeedBack feedBack = new FeedBack();
@@ -53,6 +67,22 @@ namespace Library.Inclock.web.br.BL
             MySqlAdicionaParametro("_periodo", expediente.Periodo);
             MySqlAdicionaParametro("_funcionario_id", expediente.Funcionario_id);
             feedBack = MySqlExecutaComando("prd_updade_expediente", System.Data.CommandType.StoredProcedure);
+
+            if (feedBack.Mensagem.Contains("integral"))
+            {
+                feedBack.Mensagem = "Nesse expediente tem um periodo integral";
+                feedBack.Status = false;
+            }
+            else if (feedBack.Mensagem.Contains("duplicate"))
+            {
+                feedBack.Status = false;
+                feedBack.Mensagem = "Expediente ja cadastrado";
+            }
+            else if (feedBack.Mensagem.Contains("expediente"))
+            {
+                feedBack.Status = false;
+                feedBack.Mensagem = "Você não pode cadastrar um expediente integral no dia que ja tem outros expedientes cadastrados";
+            }
             return feedBack;
         }
         public bool Excluir(int id)
@@ -74,7 +104,6 @@ namespace Library.Inclock.web.br.BL
             hora = hora.Add(ht);
             if (hora.Day > DateTime.Now.Day)  // Se virar o dia, quer dizer que o func vai bater a saida no outro dia         
                 expediente.DiaSemana++;
-
             return expediente.DiaSemana;
         }
 
